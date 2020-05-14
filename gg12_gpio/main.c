2a1aa6eca7a61f6337d1ebcedc2823606a10b526
changpeng <1293113129@qq.com>
2020-05-14 11:33:01 -0500

    using c to enable lights

--- gg12_gpio/main.c
+++ gg12_gpio/main.c
@@ -6,18 +6,190 @@
 
 #define COUNT_UP_VALUE              (1000000)
 
+void state00 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x1000;
+}
+
+void state01 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x2000;
+}
+
+void state02 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x4000;
+}
+
+void state03 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x3000;
+}
+
+void state04 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x5000;
+}
+
+void state05 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x6000;
+}
+
+void state06 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+	GPIO->P[0].DOUT ^= 0x7000;
+}
+
+void state07 () {
+	GPIO->P[0].DOUT |= 0xFFFF;
+}
+
+
+
+void state10 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[3].DOUT ^= 0x0040;
+}
+
+void state11 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT ^= 0x1000;
+}
+
+void state12 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT ^= 0x1000;
+}
+
+void state13 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[3].DOUT ^= 0x0040;
+	GPIO->P[4].DOUT ^= 0x1000;
+}
+
+void state14 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[3].DOUT ^= 0x0040;
+	GPIO->P[5].DOUT ^= 0x1000;
+}
+
+void state15 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT ^= 0x1000;
+	GPIO->P[5].DOUT ^= 0x1000;
+}
+
+void state16 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+	GPIO->P[3].DOUT ^= 0x0040;
+	GPIO->P[4].DOUT ^= 0x1000;
+	GPIO->P[5].DOUT ^= 0x1000;
+}
+
+void state17 () {
+	GPIO->P[3].DOUT |= 0xFFFF;
+	GPIO->P[4].DOUT |= 0xFFFF;
+	GPIO->P[5].DOUT |= 0xFFFF;
+}
+
 int main (void) {
     // Enable GPIO HFBUSCLK.
     CMU->HFBUSCLKEN0 |= 0x10;
     // Clear PA12 bit field.
-    GPIO->P[0].MODEH &= ~0x000F0000;
     // Set PA12 to Push-pull output.
+    GPIO->P[0].MODEH &= 0xFFF0FFFF;
     GPIO->P[0].MODEH |= 0x00040000;
+
+    GPIO->P[0].MODEH &= 0xFF0FFFFF;
+    GPIO->P[0].MODEH |= 0x00400000;
+
+    GPIO->P[0].MODEH &= 0xF0FFFFFF;
+    GPIO->P[0].MODEH |= 0x04000000;
+
+    GPIO->P[3].MODEL &= 0xF0FFFFFF;
+    GPIO->P[3].MODEL |= 0x04000000;
+
+    GPIO->P[4].MODEH &= 0xFFF0FFFF;
+    GPIO->P[4].MODEH |= 0x00040000;
+
+    GPIO->P[5].MODEH &= 0xFFF0FFFF;
+    GPIO->P[5].MODEH |= 0x00040000;
+
+    GPIO->P[3].MODEL &= 0xFF0FFFFF;
+    GPIO->P[3].MODEL |= 0x00100000;
+
+    GPIO->P[3].MODEH &= 0xFFFFFFF0;
+    GPIO->P[3].MODEH |= 0x00000001;
+
+    GPIO->P[0].DOUT |= 0xFFFFFFFF;
+    GPIO->P[3].DOUT |= 0xFFFFFFFF;
+    GPIO->P[4].DOUT |= 0xFFFFFFFF;
+    GPIO->P[5].DOUT |= 0xFFFFFFFF;
+
+
+    int count1 = 0;
+    int count2 = 0;
     // Loop
     while (1) {
         // Delay.
         for(volatile int i = 0; i < COUNT_UP_VALUE; i++);
         // Toggle PA12.
-        GPIO->P[0].DOUT ^= 0x1000;
+        if (GPIO->P[3].DIN == 0x140 || GPIO->P[3].DIN == 0x100) {
+        	if (count1 == 0) {
+        		state00();
+        	} else if (count1 == 1) {
+        		state01();
+        	} else if (count1 == 2) {
+        		state02();
+        	} else if (count1 == 3) {
+        		state03();
+        	} else if (count1 == 4) {
+        		state04();
+        	} else if (count1 == 5) {
+        		state05();
+        	} else if (count1 == 6) {
+        		state06();
+        	} else if (count1 == 7) {
+        		state07();
+        		count1 = -1;
+        	}
+        	count1++;
+        }
+        if (GPIO->P[3].DIN == 0x60 || GPIO->P[3].DIN == 0x20) {
+        	if (count2 == 0) {
+        		state10();
+        	} else if (count2 == 1) {
+        		state11();
+        	} else if (count2 == 2) {
+        		state12();
+        	} else if (count2 == 3) {
+        		state13();
+        	} else if (count2 == 4) {
+        		state14();
+        	} else if (count2 == 5) {
+        		state15();
+        	} else if (count2 == 6) {
+        		state16();
+        	} else if (count2 == 7) {
+        		state17();
+        		count2 = -1;
+        	}
+        	count2++;
+        }
     }
 }
---
+++ gg12_gpio_asm_test/main.s
@@ -0,0 +1,70 @@
+/*
+ * @file main.s
+ */
+    .section .text, "ax"                    // allocatable and executable
+    .balign 4                               // 4 bytes alignment, why?
+    .syntax unified                         // Unified syntax between ARM and THUMB
+    .thumb                                  // THUMB instructions
+
+    /* Register Address Macros */
+    .equ CMU_BASE,                  (0x400E4000UL)
+    .equ CMU_HFBUSCLKEN0_OFFSET,    (0x0B0)
+    .equ CMU_HFBUSCLKEN0,           (CMU_BASE + CMU_HFBUSCLKEN0_OFFSET)
+    .equ GPIO_BASE,					(0x40088000UL)
+    .equ GPIO_PA_MODEH_OFFSET,      (0x008)
+    .equ GPIO_PA_MODEH,				(GPIO_BASE + GPIO_PA_MODEH_OFFSET)
+    .equ GPIO_PA_DOUT_OFFSET,       (0x00C)
+    .equ GPIO_PA_DOUT,              (GPIO_BASE + GPIO_PA_DOUT_OFFSET)
+    /* Application Macros */
+	.equ COUNT_UP_VALUE,            (1000000)
+    /* Global Functions */
+    .global main
+
+// initialize buttons and LEDs
+gpio_init:
+    /* Enable High Frequency Bus Clock for GPIO. */
+    LDR         R0, =CMU_HFBUSCLKEN0        // Get address of CMU HFBUSCLKEN0 register.
+    LDR         R1, [R0]                    // Get value of CMU HFBUSCLKEN0 register.
+    ORR         R1, 0x10                    // Enable GPIO HFBUSCLK.
+    STR			R1, [R0]                    // Write result back to CMU HFBUSCLKEN0 register.
+    /* enable LED0R (PA12). */
+    LDR         R0, =GPIO_PA_MODEH          // Get address of GPIO Port A MODEH register.
+    LDR         R1, [R0]                    // Get value of GPIO Port A MODEH register.
+    AND         R1, 0xFFF0FFFF              // Clear PA12 bit field.
+    ORR         R1, 0x00040000              // Set PA12 to Push-pull output.
+    STR         R1, [R0]                    // Write result back to GPIO Port A MODEH register.
+    /* Enable LED0B (PA13). */
+
+    /* Enable LED0G (PA14). */
+
+    /* Enable LED1R (PD6). */
+
+    /* Enable LED1B (PE12). */
+
+    /* Enable LED1G (PF12). */
+
+    /* Enable Push Button 0 (PD5). */
+
+    /* Enable Push Button 1 (PD8). */
+
+    BX          LR
+
+// Main function
+main:
+    BL          gpio_init                   // Initialize GPIO.
+    /* GPIO Operations. */
+    LDR         R0, =GPIO_PA_DOUT           // Get address of GPIO Port A DOUT register.
+    LDR         R1, [R0]                    // Get value of GPIO Port A DOUT register.
+    LDR         R3, =COUNT_UP_VALUE         // Set count up value.
+loop:                                       // Main loop.
+    EOR         R1, 0x1000                  // Toggle LED value.
+    MOVS        R2, #0x0                    // Clear R2.
+delay:                                      // Delay by counting up.
+    ADDS        R2, #0x1                    // R2 += 1.
+    CMP         R2, R3                      // Set condition by comparing R2 to R3.
+	BNE         delay                       // If still counting, back to delay label.
+    STR         R1, [R0]                    // Set LED0.
+    B           loop                        // Back to loop.
+
+    .align
+    .end
